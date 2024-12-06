b401a6e20fd4dfeaa0397eb9e2db78afa83e1981
davifariasp <davi.informatica8@gmail.com>
2024-12-05 22:49:55 -0300

    feature: promocoes

--- supermarket-system/src/main/java/com/example/supermarket_system/models/Purchase.java
+++ supermarket-system/src/main/java/com/example/supermarket_system/models/Purchase.java
@@ -1,5 +1,8 @@
 package com.example.supermarket_system.models;
 
+import com.example.supermarket_system.models.promotions.BuyXGetYFreePromotion;
+import com.example.supermarket_system.models.promotions.FlatPercentPromotion;
+import com.example.supermarket_system.models.promotions.QuantityBasedPriceOverridePromotion;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,5 +22,68 @@ public class Purchase {
         this.products = products;
         this.total = products.stream().map(PurchaseItem::getPrice).reduce(0, Integer::sum);
         this.discount = 0;
+    }
+
+    public void availablePromotions(){
+
+        products.forEach(product -> {
+            List<Promotion> promotions = product.getPromotions();
+            promotions.forEach(promotion -> {
+                applyPromotion(product, promotion);
+            });
+        });
+
+        //a cada x produtos o valor deles passa a ser y
+        //        "promotions": [
+        //        {
+        //            "id": "ibt3EEYczW",
+        //                "type": "QTY_BASED_PRICE_OVERRIDE",
+        //                "required_qty": 2,
+        //                "price": 1799
+        //        }
+        //      ]
+
+        //cada produto recebe um desconto
+        //        "promotions": [
+        //        {
+        //            "id": "Gm1piPn7Fg",
+        //                "type": "FLAT_PERCENT",
+        //                "amount": 10
+        //        }
+        //      ]
+
+        //cada x produtos um passa a ser gratuito
+        //        "promotions": [
+        //        {
+        //            "id": "ZRAwbsO2qM",
+        //                "type": "BUY_X_GET_Y_FREE",
+        //                "required_qty": 2,
+        //                "free_qty": 1
+        //        }
+        //      ]
+    }
+
+    public void applyPromotion(PurchaseItem purchaseItem, Promotion promotion){
+
+        if(promotion instanceof QuantityBasedPriceOverridePromotion qtyPromotion){
+
+            //quantas vezes pode ser aplicada (tem q ser um resultado inteiro)
+            int xApply = purchaseItem.getQuantity() / qtyPromotion.getRequiredQuantity();
+
+            int priceApply = qtyPromotion.getPrice();
+
+            //se no caso se applicar só uma vez, ao inves de ser X vai ser Y
+            for (int i = 0; i < xApply; i++) {
+                //product.setPrice(priceApply);
+            }
+        }
+
+        if(promotion instanceof FlatPercentPromotion flatPromotion){
+
+        }
+
+        if(promotion instanceof BuyXGetYFreePromotion buyPromotion){
+
+        }
     }
 }
