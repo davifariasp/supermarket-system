6657ead032f860d6eaf3e8c0413479997c49580b
davifariasp <davi.informatica8@gmail.com>
2024-12-07 11:20:00 -0300

    fix: asjutando model product

--- api/src/main/java/com/example/supermarket_system/models/Product.java
+++ api/src/main/java/com/example/supermarket_system/models/Product.java
@@ -4,6 +4,7 @@ package com.example.supermarket_system.models;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     private String id;
     private String name;
